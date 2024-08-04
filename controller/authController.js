eacc5fcdcfbcd94bd2146a8dc0c9ab20b3e7d518
iamjoker021 <iamjoker021@gmail.com>
2024-08-04 18:36:55 +0530

    feat: Add post response in Router and added dummy controller fn to support the same

--- controller/authController.js
+++ controller/authController.js
@@ -2,11 +2,23 @@ const signupPage = (req, res) => {
     res.render('sign-up');
 }
 
+const addUser = (req, res) => {
+    const { username, password } = req.body;
+    res.json({username, password, 'msg': 'To add user'});
+}
+
 const loginPage = (req, res) => {
     res.render('login');
 }
 
+const validateUser = (req, res) => {
+    const { username, password } = req.body;
+    res.json({username, password, 'msg': 'To validate User Login'})
+}
+
 module.exports = {
     signupPage,
-    loginPage
+    loginPage,
+    addUser,
+    validateUser
 }
--- router/authRouter.js
+++ router/authRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { signupPage, loginPage } = require('../controller/authController');
+const { signupPage, loginPage, addUser, validateUser } = require('../controller/authController');
 
 const authRouter = Router();
 
 authRouter.get('/', (req, res) => res.json({'msg': 'working'}));
 authRouter.get('/sign-up', signupPage);
+authRouter.post('/sign-up', addUser);
 authRouter.get('/login', loginPage);
+authRouter.post('/login', validateUser);
 
 module.exports = authRouter;
